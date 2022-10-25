a54fc65fe5d92308aee1a73546053b8b966b339d
Jakub Balda <kuber579@gmail.com>
2022-10-25 15:13:35 +0200

    Added logging in page and security

--- src/main/java/com/creditcalc/creditcalc/routing.java
+++ src/main/java/com/creditcalc/creditcalc/routing.java
@@ -1,13 +1,44 @@
 package com.creditcalc.creditcalc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class routing {
 
     @GetMapping("/home")
-    public String get(){
-        return "home";
+    public String get(HttpSession session){
+            if(session.getAttribute("role") == null){
+                return "login";
+            }else{
+                return "home";
+            }
+    }
+
+    @PostMapping("/logging")
+    public String logging(HttpSession session, @RequestParam("login") String login, @RequestParam("password") String password, Model model){
+        String loginDTO = "Baldzik";
+        String passwordDTO = "Baldiko123";
+
+        if(login.equals(loginDTO) && password.equals(passwordDTO)){
+            session.setAttribute("role", "User");
+            session.setAttribute("login", login);
+
+            return "home";
+        }else{
+            model.addAttribute("message", "Unable to login, wrong password or login");
+            return "login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("role");
+        return "login";
     }
 }
