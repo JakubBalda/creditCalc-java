369a774a46bc23a80abf032639c1ed51f02d4cf3
Jakub Balda <kuber579@gmail.com>
2022-11-14 18:29:59 +0100

    logging from database

--- src/main/java/com/creditcalc/creditcalc/routing.java
+++ src/main/java/com/creditcalc/creditcalc/routing.java
@@ -1,5 +1,7 @@
 package com.creditcalc.creditcalc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @Controller
 public class routing {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping("/home")
     public String get(HttpSession session){
@@ -22,13 +32,34 @@ public class routing {
 
     @PostMapping("/logging")
     public String logging(HttpSession session, @RequestParam("login") String login, @RequestParam("password") String password, Model model){
-        String loginDTO = "Baldzik";
-        String passwordDTO = "Baldiko123";
+        String username = null;
+        String pass = null;
+        String role = null;
 
-        if(login.equals(loginDTO) && password.equals(passwordDTO)){
-            session.setAttribute("role", "User");
-            session.setAttribute("login", login);
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/creditcalcjava", "root", "");
 
+            String query = "SELECT Username, Password, Role FROM Users WHERE Username = ?";
+
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, login);
+            ResultSet rs = preparedStatement.executeQuery();
+
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                username = rs.getString(1);
+                pass = rs.getNString(2);
+                role = rs.getString(3);
+            }
+
+        }catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+
+        if(username.equals(login) && password.equals(pass)){
+            session.setAttribute("role", role);
             return "home";
         }else{
             model.addAttribute("message", "Unable to login, wrong password or login");
